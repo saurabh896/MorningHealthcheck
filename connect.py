c17ba552f33f4e775e645b94508472349b1d4f65
Saurabh Kadam <saurabhkdm721@gmail.com>
2019-03-30 22:46:23 +0530

    Changing config

--- connect.py
+++ connect.py
@@ -1,10 +1,14 @@
 import pyodbc
 
-connection = pyodbc.connect("DSN=USER-PC,autocommit=True")
+connection = pyodbc.connect('DRIVER={ODBC Driver 13 for SQL Server};SERVER=localhost;UID=userdev;PWD=Halls12345$')
 
 if connection:
     print("Yes we are connected")
 else:
     print("Not connected")
+cursor = connection.cursor()
+cursor.execute('select  * from spt_monitor')
 
+for row in cursor:
+    print('My Records = %r' % (row))
 
